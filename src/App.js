d3577f51fd8c004d5b82a71f81cf33d238c9634e
Tarun 2 <141573811+tarunmandhan2@users.noreply.github.com>
2025-01-21 14:02:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import VirtualizedList from "./VirtualizedList";
+
+export default function App() {
+  const data = Array.from({ length: 1000 }, (_, i) => ` Product ${i + 1} `);
+  console.log(data);
+
+  return (
+    <div className="App">
+      <h1>13. Implement a Virtualized List</h1>
+      <VirtualizedList data={data} />
+    </div>
+  );
+}
---
+++ src/VirtualizedList.jsx
@@ -0,0 +1,8 @@
+const VirtualizedList = ({ data }) => {
+  return (
+    <>
+      <h1>{data}</h1>
+    </>
+  );
+};
+export default VirtualizedList;
